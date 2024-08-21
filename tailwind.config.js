b7c986266c283cd974da5c9bc56044c2b6af0df4
SWAPNIL AHMED SHISHIR <ahmed66882298@gmail.com>
2024-08-21 23:20:26 +0600

    header part design done

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        colors: {
+          '212121': '#243c5a',
+          '494949': '#494949',
+          '737373': '#737373',
+          'ABEF5F': '#ABEF5F',
+          '1C1C1C': '#1C1C1C',
+        },
+      },
+    },
+    plugins: [],
+  }
