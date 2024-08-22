ae9628a94389236f8eeb2909df270a6719438a9d
SWAPNIL AHMED SHISHIR <ahmed66882298@gmail.com>
2024-08-22 20:33:20 +0600

    assinngmet 3 works done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
+    content: ["*.{html,js}"],
     theme: {
-      extend: {
-        colors: {
-          '212121': '#243c5a',
-          '494949': '#494949',
-          '737373': '#737373',
-          'ABEF5F': '#ABEF5F',
-          '1C1C1C': '#1C1C1C',
-        },
-        fontFamily: {
-          'manrope': ['"Manrope"', "sans-serif"],
-        },
+      extend: {      
         
       },
     },
